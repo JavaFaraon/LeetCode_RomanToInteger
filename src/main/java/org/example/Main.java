a5d80a4c0f7ace29de1ca5455effda0df609b1ee
JavaFaraon <ivan@rybakovka.com>
2023-02-12 17:15:21 +0200

    Rename

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,42 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().run();
+    }
+
+    public void run() {
+        Solution solution = new Solution();
+        System.out.println("Result = " + solution.romanToInt(inputString()));
+    }
+
+    public String inputString() {
+        System.out.println("Введите римское число в виде строки БОЛЬШИМИ БУКВАМИ");
+        Scanner scan = new Scanner(System.in);
+        String s;
+        if (scan.hasNextLine()) {
+            s = scan.nextLine();
+            if (!Objects.equals(s, "")) {
+              if (!isMatch(Pattern.compile("^(M{0,3})(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,3}|I[VX])$"), s)) {
+                  System.out.println("Вы ввели число в неправильном формате");
+                  return  inputString();
+              } else System.out.println("Вы ввели " + s);
+            } else {
+                return inputString();
+            }
+        } else {
+            return inputString();
+        }
+        return s;
+    }
+
+    public boolean isMatch(Pattern ptr, String str) {
+        Matcher matcher = ptr.matcher(str);
+        return matcher.find();
     }
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,27 +4,25 @@ import java.sql.Array;
 
 public class Solution {
     public int romanToInt(String s) {
-        char [] chars = s.toLowerCase().toCharArray();
+        char [] chars = s.toCharArray();
         int result = 0;
         int arabic = 0;
         int arabicPrevious = 0;
         Main main = new Main();
         for (int i = chars.length -1; i >= 0 ; i--) {
             switch (chars[i]) {
-                case 'm' -> arabic = 1000;
-                case 'd' -> arabic = 500;
-                case 'c' -> arabic = 100;
-                case 'l' -> arabic = 50;
-                case 'x' -> arabic = 10;
-                case 'v' -> arabic = 5;
-                case 'i' -> arabic = 1;
+                case 'M' -> arabic = 1000;
+                case 'D' -> arabic = 500;
+                case 'C' -> arabic = 100;
+                case 'L' -> arabic = 50;
+                case 'X' -> arabic = 10;
+                case 'V' -> arabic = 5;
+                case 'I' -> arabic = 1;
                 default -> {
-                    System.out.println("Выражение ошибочно.");
-                    main.inputString();
+//                    System.out.println("Выражение ошибочно.");
+//                    main.inputString();
                 }
             }
-            System.out.println("Arabic = " + arabic);
-
             if (arabic >= arabicPrevious) {
                 result+=arabic;
             } else result-=arabic;
